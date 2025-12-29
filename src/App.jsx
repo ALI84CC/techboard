1ace586f94847cba91c3435f303a8ecc842c9976
alisonCosta <alison84cc@gmail.com>
2025-12-29 14:07:08 -0300

    intetgração dos repositórios

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <main>
+      <header>
+         <img src='/Logo.png' alt='Logo da Aplicação'/>
+      </header>
+      <section>
+         <img src='/Banner.png' alt='Banner da Aplicação'/>
+       </section>
+      
+    </main>
+  )
+}
+
+export default App
